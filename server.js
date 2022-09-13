b65bfad8cea42de2fa2a4ff8b8cabcf851186c4a
Ville Laaksonen <leligens@gmail.com>
2022-09-13 11:56:43 +0300

    added cors and error handler middleware

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const cors = require("cors");
 const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
 app.use(logger);
+
+// Cross Origin Resource Sharing
+const whitelist = ["https://www.yoursite.com", "http://127.0.0.1:3000", "http://localhost:3000"];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    optionsSuccessStatus: 200 
+}
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -25,8 +41,17 @@ app.get("/old-page", (req, res) => {
     res.redirect(301, "new-page");
 });
 
-app.get("/*", (req, res) => {
-    res.status(404).appendsendFile(path.join(__dirname, "views", "404.html"));
+app.all("*", (req, res) => {
+    res.status(404);
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "views", "404.html"));
+    } else if (req.accepts("json")) {
+        res.json({ error: "404 Not Found" });
+    } else {
+        res.type("txt").send("404 Not Found");
+    }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
