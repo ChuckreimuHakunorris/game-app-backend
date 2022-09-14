76f9bf67d0bf016bb45e11144e6aac063de4194c
Ville Laaksonen <leligens@gmail.com>
2022-09-14 11:11:04 +0300

    register controller logic

---
+++ controllers/registerController.js
@@ -0,0 +1,40 @@
+const playersDB = {
+    players: require("../model/players.json"),
+    setPlayers: function (data) { this.players = data}
+}
+
+const fsPromises = require("fs").promises;
+const path = require("path");
+const bcrypt = require("bcrypt");
+
+const handleNewPlayer = async (req, res) => {
+    const { player, pwd } = req.body;
+    if (!player || !pwd) return res.status(400).json({
+        "message": "Username and password are required."
+    });
+
+    // Check for duplicate usernames in the db
+    const duplicate = playersDB.players.find(user => user.username === player);
+
+    if (duplicate) {
+        return res.sendStatus(409); // Conflict
+    }
+
+    try {
+        // Encrypt the password
+        const hashedPwd = await bcrypt.hash(pwd, 10);
+        // Store the new player
+        const newPlayer = { "username": player, "password": hashedPwd };
+        playersDB.setPlayers([...playersDB.players, newPlayer]);
+        await fsPromises.writeFile(
+            path.join(__dirname, "..", "model", "players.json"),
+            JSON.stringify(playersDB.players)
+        );
+        console.log(playersDB.players);
+        res.status(201).json({ "success": `New player ${player} created!` });
+    } catch (err) {
+        res.status(500).json({ "message": err.message });
+    }
+}
+
+module.exports = { handleNewPlayer };
