0a7ca5424f9f89e909eec5b65e5bfe75ca08c7e5
Ville Laaksonen <leligens@gmail.com>
2022-09-22 12:08:57 +0300

    added more entries to the origins whitelist

--- config/whitelist.js
+++ config/whitelist.js
@@ -1,4 +1,8 @@
 const whitelist = [
+    "https://castrum-tactics.netlify.app/login", 
+    "https://castrum-tactics.netlify.app/register", 
+    "https://castrum-tactics.netlify.app/home",
+    "https://castrum-tactics.netlify.app/admin",  
     "https://castrum-tactics.netlify.app", 
     "http://127.0.0.1:3000",
     "http://localhost:3000"
